cb2214772af80144ada76aaab7763eb8ee4bd541
Kostas Mantzavelas <kostiksm@gmail.com>
2018-12-08 15:49:01 +0200

    Initial server-side commit

---
+++ tripassistant-api/src/main/java/com/mantzavelas/tripassistantapi/TripassistantApiApplication.java
@@ -0,0 +1,12 @@
+package com.mantzavelas.tripassistantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripassistantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripassistantApiApplication.class, args);
+	}
+}
