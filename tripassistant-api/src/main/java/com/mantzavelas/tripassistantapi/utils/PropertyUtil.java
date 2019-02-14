ff47e649cb6b43069f74b6e1d5cc75ed0d2176e0
Kostas Mantzavelas <kostiksm@gmail.com>
2019-02-14 21:47:38 +0200

    Minor fixes to PropertyUtil. Now loading application.properties through Spring context.

--- tripassistant-api/src/main/java/com/mantzavelas/tripassistantapi/utils/PropertyUtil.java
+++ tripassistant-api/src/main/java/com/mantzavelas/tripassistantapi/utils/PropertyUtil.java
@@ -1,26 +1,17 @@
 package com.mantzavelas.tripassistantapi.utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class PropertyUtil {
 
-	private static Properties applicationProperties;
+	private static PropertyLoader properties;
+
 	private static void initialize() {
-		if (applicationProperties == null) {
-			applicationProperties = new Properties();
-			try {
-				applicationProperties.load(new FileInputStream("src/main/resources/application.properties"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		if (properties == null) {
+			properties = BeanUtil.getBean(PropertyLoader.class);
 		}
 	}
 
 	public static String getProperty(String property) {
 		initialize();
-		return applicationProperties.getProperty(property);
+		return properties.getProperty(property);
 	}
-
 }
